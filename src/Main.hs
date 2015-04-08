0af3b68e6ea3d86aa7ae0886b518fb71ed3309e0
Christine Dodrill <xena@yolo-swag.com>
2015-04-07 22:39:09 -0700

    Add improvments from imran

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,24 @@
 import Control.Concurrent
+import Control.Monad
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import Data.ByteString.Char8
+
+{-# LANGUAGE OverloadedStrings #-}
 
 main :: IO ()
 main = do
     sock <- socket AF_INET Stream 0
     bindSocket sock (SockAddrInet 8430 iNADDR_ANY)
-    listen sock 2
+    listen sock sOMAXCONN
     mainLoop sock
 
 mainLoop :: Socket -> IO ()
-mainLoop sock = do
+mainLoop sock = forever $ do
     (client,_) <- accept sock
     forkIO $ do
         send client payload
         sClose client
-    mainLoop sock
-
 
 payload :: ByteString
-payload = pack "<?xml version=\"1.0\"?>\
-               \<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\
-               \<cross-domain-policy>\
-               \    <site-control permitted-cross-domain-policies=\"master-only\"/>\
-               \    <allow-access-from domain=\"*\" to-ports=\"6667\" />\
-               \</cross-domain-policy>"
+payload = "<?xml version=\"1.a\"?> <!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\"><cross-domain-policy>  <site-control permitted-cross-domain-policies=\"master-only\"/><allow-access-from domain=\"*\" to-ports=\"6667\" /></cross-domain-policy>"
