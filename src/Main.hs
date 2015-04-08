1cbaa92d7cd57b9f4a5df07e068383d80eb0cc75
Christine Dodrill <xena@yolo-swag.com>
2015-04-08 09:05:07 -0700

    refactor out mainLoop

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Concurrent
 import Control.Monad
 import Text.Printf
@@ -8,17 +10,14 @@ import Data.ByteString.Char8
 main :: IO ()
 main = do
     sock <- socket AF_INET Stream 0
-    bindSocket sock (SockAddrInet 8430 iNADDR_ANY)
+    bindSocket sock (SockAddrInet 8430 iNADDR_ANY) -- 8430 is de-facto standard for flash policy daemons
     listen sock sOMAXCONN
-    mainLoop sock
-
-mainLoop :: Socket -> IO ()
-mainLoop sock = forever $ do
-    (client,host) <- accept sock
-    forkIO $ do
-        printf "connection from %s\n" (show host)
-        send client payload
-        sClose client
+    forever $ do
+      (client,host) <- accept sock
+      forkIO $ do
+          printf "connection from %s\n" (show host)
+          send client payload
+          sClose client
 
 payload :: ByteString
 payload = pack "<?xml version=\"1.a\"?><!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\"><cross-domain-policy><site-control permitted-cross-domain-policies=\"master-only\"/><allow-access-from domain=\"*\" to-ports=\"6667\" /></cross-domain-policy>"
